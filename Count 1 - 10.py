eec9a8fd00c605edc2a274f212b28a80d8ba055d
Burke <02john.burke@gulllakecs.org>
2024-03-12 11:34:19 -0700

    counts 1 through 10 / stable

--- Count 1 - 10.py
+++ Count 1 - 10.py
@@ -1,4 +1,8 @@
 # Programmer: John Burke
-#  Date: 3/12/24
-# # Program: Count 1-10
-# # Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+# Date: 3/12/24
+# Program: Count 1-10
+# Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+# Loop from 1 to 10 and print each number
+for i in range(1, 11):
+    print(i)
