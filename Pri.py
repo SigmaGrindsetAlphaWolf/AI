2513ce86e2dc9d4393ee87ae4a3ea967033a960b
Burke <02john.burke@gulllakecs.org>
2024-03-14 11:02:26 -0700

    Printing names index - stable Pre ChatGPT

--- Pri.py
+++ Pri.py
@@ -2,3 +2,10 @@
 # Date: 3/12/2024
 # Program: Password Generator
 # Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+names = ("Cory","Christ","Travis","Dave")
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
